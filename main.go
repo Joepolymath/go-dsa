1bcdddb05eb0be2c4c8f8d6d2392d8362e09b38a
Joshua_Ajagbe <joshua@equally.ai>
2023-09-11 22:41:13 +0100

    binary search function call

--- main.go
+++ main.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	testArray := []int{1, 3, 5, 6, 7, 9, 12}
-	fmt.Println(LinearSearch(testArray, 5))
-	fmt.Println(BinarySearch(testArray, 5))
+	fmt.Println(LinearSearch(testArray, 5)) // should return true
+	fmt.Println(BinarySearch(testArray, 2)) // should return false
 }
