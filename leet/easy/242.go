99418cee6b29c17ce1729ae438f0e9a1572e1f40
Joshua_Ajagbe <joshuaajagbe96@gmail.com>
2024-01-21 01:19:50 +0100

    leetcode 242 solved

---
+++ leet/easy/242.go
@@ -0,0 +1,41 @@
+package leet
+
+import (
+	"strings"
+)
+
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+
+    sMap := make(map[string]int)
+    tMap := make(map[string]int)
+
+    sArray := strings.Split(s, "")
+    tArray := strings.Split(t, "")
+
+    for _, char := range sArray {
+        if _, found := sMap[char]; found {
+            sMap[char]++
+        } else {
+            sMap[char] = 1
+        }
+    }
+
+    for _, char := range tArray {
+        if _, found := tMap[char]; found {
+            tMap[char]++
+        } else {
+            tMap[char] = 1
+        }
+    }
+
+    // comparing the two maps
+    for idx := range sMap {
+        if sMap[idx] != tMap[idx] {
+            return false
+        }
+    }
+    return true
+}
