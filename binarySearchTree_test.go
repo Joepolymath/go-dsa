ecbc7b379241dafb8794d3f36db060f472cb3d49
Joshua_Ajagbe <joshuaajagbe96@gmail.com>
2024-02-13 03:08:49 +0100

    test cleanup

--- binarySearchTree_test.go
+++ binarySearchTree_test.go
@@ -20,21 +20,3 @@ func Test_Insert_BinarySearchTree(t *testing.T) {
 		t.Logf(TestSuccessResponse(expected, result))
 	}
 }
-
-// func Test_Pop_StackWithLinkedList(t *testing.T) {
-// 	stack := &datastructures.Stack2{
-// 		MaxSize: 5,
-// 	}
-// 	stack.Push(55)
-// 	expected := 55
-// 	result, err := stack.Pop()
-// 	if err != nil {
-// 		t.Errorf("FAILED: Push method threw an error %v", err)
-// 	}
-
-// 	if result != expected {
-// 		t.Errorf(TestErrorResponse(expected, result))
-// 	} else {
-// 		t.Logf(TestSuccessResponse(expected, result))
-// 	}
-// }
